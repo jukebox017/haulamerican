60181ede242509025d84aa9bcddb6c1f76012ded
Daniel Lineberger <ace017@gmail.com>
2026-01-06 23:31:26 -0700

    Enable github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://jukebox017.github.io',
+  base: '/haulamerican'
+});
