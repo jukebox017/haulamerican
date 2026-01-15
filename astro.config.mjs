73eaae9ba98f596b8a00688e4df1a3b4bad17f31
Daniel Lineberger <ace017@gmail.com>
2026-01-14 22:29:20 -0700

    Remove base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jukebox017.github.io',
-  base: '/haulamerican'
+  site: 'https://jukebox017.github.io'
 });
